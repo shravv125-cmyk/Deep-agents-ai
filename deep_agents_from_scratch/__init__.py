bc2de2889e4eb4b3d83226401e2b08dc85a66844
Shravani <shravv125@gmail.com>
2026-03-24 14:26:14 +0530

    Add files via upload

---
+++ deep_agents_from_scratch/__init__.py
@@ -0,0 +1,17 @@
+
+
+def state():
+    return None
+
+
+def todo_tools():
+    return None
+
+
+def file_tools():
+    return None
+
+from .files import ls, read_file, write_file
+from .todo import write_todos, read_todos
+
+
